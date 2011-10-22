2d1640c696ea315bd958f639f8c93058c457a100
Mark Cahill <mark@thinkjson.com>
2011-10-22 19:42:56 -0400

    Started Lottery implementation

--- src/LotteryScheduling.java
+++ src/LotteryScheduling.java
@@ -1,12 +1,27 @@
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Vector;
+
 
 public class LotteryScheduling extends SchedulingQueue
 {
+	
+	static final Random random = new Random();
+	
+	HashMap<Integer, Vector<Integer>> tickets;
+
+	/**
+	 * 
+	 */
+	public LotteryScheduling() {
+		super();
+		tickets = new HashMap<Integer, Vector<Integer>>();
+	}
 
 	@Override
 	public void addProcess(Process p)
 	{
-		// TODO Auto-generated method stub
-		
+		tickets.put(p.getPID(), new Vector<Integer>());
 	}
 
 	@Override
--- src/Process.java
+++ src/Process.java
@@ -9,6 +9,10 @@ public class Process {
 	private int PID;
 	private User user;
 	
+	public int getPID() {
+		return PID;
+	}
+
 	private static final Random random = new Random();
 	private static final int MAX_TIME = 10000;
 
