602b526f117be8bacf8711b22987e92a7289d18e
jeremyy <jyanik@stetson.edu>
2011-10-22 18:54:02 -0400

    Added scheduling technique skeletons.

---
+++ src/LotteryScheduling.java
@@ -0,0 +1,19 @@
+
+public class LotteryScheduling extends SchedulingQueue
+{
+
+	@Override
+	public void addProcess(Process p)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeProcess(Process p)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/MultilevelFeedbackQueue.java
@@ -0,0 +1,19 @@
+
+public class MultilevelFeedbackQueue extends SchedulingQueue
+{
+
+	@Override
+	public void addProcess(Process p)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeProcess(Process p)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
